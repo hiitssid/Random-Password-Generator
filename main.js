dd72d3a7c80bd410aeb65b5417f79ebca76e3a54
hiitssid <see.siddhartha@gmail.com>
2022-04-23 13:27:35 +0545

    First Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+//selecting button with ID
+let button = document.getElementById("generatebtn");
+//selecting Password display area with Class
+let passMsg = document.querySelector(".result");
+//adding click event chatcher and executeing function
+let generate = button.addEventListener("click", function(e) {
+    let character =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz~!@#$%^&*()_+{}[]<>,.?/";
+
+    let rand = []; //a container for all password characters; an array
+    for (let i = 0; i < 8; i++) {
+        rand.push(character[Math.floor(Math.random() * character.length)]); //pushing 8 characters to array
+    }
+    passMsg.textContent = rand.join(""); //joining or concat the elements of array to form a string
+
+    console.log(rand.join(""));
+});
